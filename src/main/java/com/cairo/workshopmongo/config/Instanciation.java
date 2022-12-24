70b517d89c4e003fdd8f84b1332ccb32be382da6
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-12-24 16:46:37 -0300

    Data projection with DTO

--- src/main/java/com/cairo/workshopmongo/config/Instanciation.java
+++ src/main/java/com/cairo/workshopmongo/config/Instanciation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.cairo.workshopmongo.domain.Post;
 import com.cairo.workshopmongo.domain.User;
+import com.cairo.workshopmongo.dto.AuthorDTO;
 import com.cairo.workshopmongo.repository.PostRepository;
 import com.cairo.workshopmongo.repository.UserRepository;
 
@@ -33,11 +34,10 @@ public class Instanciation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("23/08/2018"), "Let's go traveling!", "We are going to São Paulo!", maria);
-		Post post2 = new Post(null, sdf.parse("23/08/2018"), "Good morning!", "I woke up happy today!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("23/08/2018"), "Let's go traveling!", "We are going to São Paulo!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/08/2018"), "Good morning!", "I woke up happy today!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
